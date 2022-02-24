069d90feaef2a09ab0ad732f24007a13b681d24b
pengu5055 <urbancmarko1@gmail.com>
2022-02-24 22:42:13 +0100

    I think crange is broken

--- DN1/main.py
+++ DN1/main.py
@@ -4,7 +4,7 @@ import cmasher as cmr
 from matplotlib.collections import LineCollection
 
 
-def eq_to_hor(alpha, delta, time, SUT0, lamb, phi, deg=0):
+def eq_to_hor(alpha, delta, time, SUT0, lamb, phi):
     """
     Convert equatorial coordinates to horizontal
     INPUTS:
@@ -21,21 +21,13 @@ def eq_to_hor(alpha, delta, time, SUT0, lamb, phi, deg=0):
     """
     time = hms2deg(time)
     H = np.deg2rad((get_LST(time, SUT0, lamb) - hms2deg(alpha)) % 360)  # Warning! Alpha is given in HMS
+    print(H)
     delta = np.deg2rad(dms2deg(delta))
     phi = np.deg2rad(phi)
     h = np.arcsin(np.sin(phi)*np.sin(delta) + np.cos(phi)*np.cos(delta)*np.cos(H))
     A = np.arccos((np.sin(delta)-np.sin(phi)*np.sin(h))/(np.cos(phi)*np.cos(h)))
 
     return np.rad2deg(A), np.rad2deg(h)
-
-
-def GMST(jd):
-    """
-    Calculate Greenwich Mean Sidereal Time
-    Input is in julian date
-    """
-    T = (jd - 2451545.0)/36525
-    return 24110.54841 + 8640184.812866*T + 0.093104 * T**2 - 0.0000062 * T**3
 
 
 def dms2deg(t):
@@ -68,7 +60,7 @@ def hms2deg(time):
         # raise ValueError("Unknown input format")
         return float(time)
 
-    return ((float(s[0]) + (float(s[1]) / 60) + float(s[2]) / 3600) * 15) % 360
+    return ((float(s[0]) + (float(s[1]) / 60) + float(s[2]) / 3600) * 15) #% 360
 
 
 def deg2hms(time):
@@ -102,6 +94,7 @@ def track_azalt(alpha, delta, time_start, time_end, bins, SUT0, lamb, phi):
     time_start = hms2deg(time_start)
     time_end = hms2deg(time_end)
     times = list(crange(time_start, time_end, 360, bins))
+    print(times)
     output = []
     for time in times:
         time = deg2hms(time)  # Hacky fix the fact that eq_to_hor takes HMS time (and instantly converts it into deg)
@@ -129,61 +122,72 @@ def crange(start, end, modulo, bin):
 obstime = "18:50:05"
 AGO_lambda = 14.5277
 AGO_phi = 46.0439
-ZeroTime = 149.912405
+# ZeroTime = 149.912405
+ZeroTime = 148.926757
 
 # ----Test with Cartes du Ciel----
 RA_sirius = "06 46 07.6"
 DEC_sirius = "-16 45 57.5"
 RA_rigel = "05 15 35.9"
-DEC_rigel = "-08 20 44.8"
+DEC_rigel = "-08 10 44.8"
 
-# print(eq_to_hor(RA_rigel, DEC_rigel, obstime, ZeroTime, AGO_lambda, AGO_phi))
+r1, r2 = eq_to_hor(RA_rigel, DEC_rigel, obstime, ZeroTime, AGO_lambda, AGO_phi)
+# print(deg2dms(r1), deg2dms(r2))
 # print(eq_to_hor(RA_sirius, DEC_sirius, obstime, ZeroTime, AGO_lambda, AGO_phi))
 
 # ----Tracking two stars----
 RA_procyon = "07 40 27.871"
 DEC_procyon = "05 10 00.74 "
-RA_betaUMi = ""
-DEC_betaUMi = ""
+RA_betaUMi = "14 50 42.32580"
+DEC_betaUMi = "74 09 19.8142"
 
-t_start = "18:00:00"
-t_end = "05:00:00"  # The next day but SUT0 by definition should stay the same
+t_start = "21:00:00"
+t_end = "00:00:00"  # The next day but SUT0 by definition should stay the same
 
 
-# azalt, times, = track_azalt(RA_procyon, DEC_procyon, t_start, t_end, 20, ZeroTime, AGO_lambda, AGO_phi)
-# az = azalt[0]
-# alt = azalt[1]
-# fig, ax = plt.subplots(subplot_kw={"projection": "polar"})
-# ax.set_rlim(bottom=90, top=0)
-# ax.scatter(az, alt, c=times, cmap="cmr.cosmic", vmin=times[-1], vmax=times[0])
-# ax.set_rmax(90)
-# ax.set_rticks([0, 15, 30, 45, 60, 75, 90])
-# ax.set_rlabel_position(-22.5)
-# ax.grid(True)
-# plt.colorbar(mappable=)
-#
-# plt.title("Azimuth and elevation of Procyon")
-# plt.show()
-
-azalt, times, = track_azalt(RA_procyon, DEC_procyon, t_start, t_end, 20, ZeroTime, AGO_lambda, AGO_phi)
+azalt, times, = track_azalt(RA_procyon, DEC_procyon, t_start, t_end, 100, ZeroTime, AGO_lambda, AGO_phi)
 az = azalt[0]
 alt = azalt[1]
-datapoints = np.array([np.deg2rad(az), alt]).T.reshape(-1, 1, 2)
+print(times)
+# Plot style 1
+# datapoints = np.array([np.deg2rad(az), alt]).T.reshape(-1, 1, 2)
+datapoints = np.array([(az), alt]).T.reshape(-1, 1, 2)
+
 segments = np.concatenate([datapoints[:-1], datapoints[1:]], axis=1)
 norm = plt.Normalize(0, 360)
 lc = LineCollection(segments, cmap="cmr.infinity_s", norm=norm)
 lc.set_array(times)
 
 # WARNING: POLAR PLOT TAKES X DATA IN RADIANS
-fig, ax = plt.subplots(subplot_kw={"projection": "polar"})
-line = ax.add_collection(lc)
-ax.set_rlim(bottom=90, top=0)
-# ax.scatter(np.deg2rad(az), alt, c=times, cmap="cmr.infinity", vmin=times[-1], vmax=times[0])
-ax.set_rmax(90)
-ax.set_rticks([0, 15, 30, 45, 60, 75, 90])
-# ax.set_rlabel_position(-22.5)
-ax.set_theta_zero_location("N")
-ax.grid(True)
-plt.colorbar(line, label=r"Time $[\degree]$")
+# fig, ax = plt.subplots(subplot_kw={"projection": "polar"})
+# line = ax.add_collection(lc)
+# ax.set_rlim(bottom=90, top=0)
+# ax.set_rmax(90)
+# ax.set_rticks([0, 15, 30, 45, 60, 75, 90])
+# # ax.set_rlabel_position(-22.5)
+# ax.set_theta_zero_location("N")
+# ax.set_theta_direction(-1)
+# ax.grid(True)
+# plt.colorbar(line, label=r"Time $[\degree]$", pad=0.075)
+# plt.title(r"Azimuth and elevation of $\beta$ UMi")
+# plt.show()
+
+# Plot style 2
+fig, ax = plt.subplots()
+# line = ax.add_collection(lc)
+for i in range(len(az)):
+    plt.scatter(az[i], alt[i], label=i)
+
+plt.xlim(0, 360)
+plt.ylim(-90, 90)
+# plt.colorbar(line, label=r"Time $[\degree]$")
 plt.title("Azimuth and elevation of Procyon")
-plt.show()
+plt.xlabel(r"Azimuth $[\degree]$")
+plt.ylabel(r"Elevation $[\degree]$")
+plt.legend()
+
+time = "21:55:00"
+r1, r2 = eq_to_hor(RA_procyon, DEC_procyon, time, ZeroTime, AGO_lambda, AGO_phi)
+print(deg2dms(r1), deg2dms(r2))
+
+# plt.show()
