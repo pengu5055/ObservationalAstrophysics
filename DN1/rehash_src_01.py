2a3760dd5fa0c427911068abe6feb5f1a2780174
pengu5055 <urbancmarko1@gmail.com>
2022-02-26 15:20:50 +0100

    Fixed with unknown correction!

--- DN1/rehash_src_01.py
+++ DN1/rehash_src_01.py
@@ -1,16 +1,21 @@
 import numpy as np
 
 
-def eq2azalt(ra, dec, t, SUT0, lamb, phi, t_0=15):
-    H = np.deg2rad((SUT0 + lamb + (t - t_0)*(366.2422/365.2422)) - ra)
+def eq2azalt(alpha, delta, t, SUT0, lamb, phi, t_0=15):
+    H = np.deg2rad(((SUT0 + lamb + (t - t_0)*(366.2422/365.2422)) - alpha) % 360)
     # LST = 225.117748  # DEBUG: Manually enter Local Star time
     # LST = 44.621590
     # H = (LST - ra) % 360
     # print("Hour angle: {}".format(np.rad2deg(H)))
-    dec = np.deg2rad(dec)
+
+    delta = np.deg2rad(delta)
     phi = np.deg2rad(phi)
-    h = np.arcsin(np.sin(phi)*np.sin(dec) + np.cos(phi)*np.cos(dec)*np.cos(H))
-    A = np.arccos((np.sin(dec) - np.sin(h)*np.sin(phi))/(np.cos(h)*np.cos(phi)))
+    h = np.arcsin(np.sin(phi) * np.sin(delta) + np.cos(phi) * np.cos(delta) * np.cos(H))
+    A = np.arccos((np.sin(delta) - np.sin(h) * np.sin(phi)) / (np.cos(h) * np.cos(phi)))
+
+    # Unknown correction
+    if np.rad2deg(H) < 180:
+        return 360 - np.rad2deg(A), np.rad2deg(h)
 
     return np.rad2deg(A), np.rad2deg(h)
 
