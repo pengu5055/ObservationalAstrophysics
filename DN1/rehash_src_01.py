df7cc832ac32a0f0f7903c6476fb3fa624eb5326
pengu5055 <urbancmarko1@gmail.com>
2022-02-26 15:34:53 +0100

    trackstar function

--- DN1/rehash_src_01.py
+++ DN1/rehash_src_01.py
@@ -2,12 +2,19 @@ import numpy as np
 
 
 def eq2azalt(alpha, delta, t, SUT0, lamb, phi, t_0=15):
-    H = np.deg2rad(((SUT0 + lamb + (t - t_0)*(366.2422/365.2422)) - alpha) % 360)
-    # LST = 225.117748  # DEBUG: Manually enter Local Star time
-    # LST = 44.621590
-    # H = (LST - ra) % 360
-    # print("Hour angle: {}".format(np.rad2deg(H)))
+    """
+    Convert equatorial coordinates to alt az
+    :param alpha: Right ascension in deg
+    :param delta: Declination in deg
+    :param SUT0: Greenwich mean sidereal time at 0h UT in deg
+    :param t: Time of observation in deg
+    :param lamb: Observer longitude in deg
+    :param phi: Observer latitude in deg
 
+    :return A: Azimuth
+    :return h: Elevation
+    """
+    H = np.deg2rad(((SUT0 + lamb + (t - t_0)*(366.2422/365.2422)) - alpha) % 360)
     delta = np.deg2rad(delta)
     phi = np.deg2rad(phi)
     h = np.arcsin(np.sin(phi) * np.sin(delta) + np.cos(phi) * np.cos(delta) * np.cos(H))
@@ -27,3 +34,25 @@ def deg2dms(time):
 
     return "{:0>2d}:{:0>2d}:{:05.2f}".format(int(tH), int(tM), (tS))
 
+
+def trackstar(alpha, delta, t_start, t_end, bins, SUT0, lamb, phi):
+    """
+    Track elevation and azimuth of desired star at discrete time points
+    :param alpha: Right ascension of star
+    :param delta: Declination of star
+    :param t_start: Time of observation start in HMS
+    :param t_end: Time of observation end in HMS
+    :param bins: Discrete divisions of time interval (2 less than given because broken bins system in generator)
+    :param SUT0: Greenwich sidereal time at 0h UT
+    :param lamb: Observer longitude
+    :param phi: Observer latitude
+
+    :return: 2D array with Azimuth and elevation and 1D array of times
+    """
+    step = np.abs(t_start - t_end)/bins
+    times = np.arange(t_start, t_end + 360, step)
+    output = []
+    for time in times:
+        output.append(eq2azalt(alpha, delta, time, SUT0, lamb, phi))
+
+    return np.column_stack(np.array(output))
