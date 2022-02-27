192bce8370308b12b8fa0d5ae15f5feba23e6065
pengu5055 <urbancmarko1@gmail.com>
2022-02-27 15:36:46 +0100

    Plot but broken

--- DN1/rehash_proper_analemma_01.py
+++ DN1/rehash_proper_analemma_01.py
@@ -11,6 +11,26 @@ ZeroTime = 148.926757
 
 # ---- Analemma 1 ----
 GMST = np.genfromtxt("GMST_2022.tsv", usecols=(3,))
+JD_init = 2459659.00000  # Julian date of first entry in GMST
 # INFO: Table generated so, that index corresponds to days from spring equinox
 timeangle = 165  # 11AM
+data = sun_proper_analemma(GMST, timeangle, AGO_lambda, AGO_phi, JD_init)
+
+datapoints = np.array([data[0], data[1]]).T.reshape(-1, 1, 2)
+segments = np.concatenate([datapoints[:-1], datapoints[1:]], axis=1)
+norm = plt.Normalize(0, 360)
+lc = LineCollection(segments, cmap="cmr.infinity_s", norm=norm)
+lc.set_array(data[2])
+
+fig, ax = plt.subplots()
+line = ax.add_collection(lc)
+
+plt.xlim(0, 360)
+plt.ylim(-90, 90)
+plt.colorbar(line, label=r"Days since vernal equinox")
+plt.title("Corrected analemma on Earth in 2022/23")
+plt.xlabel(r"Azimuth $[\degree]$")
+plt.ylabel(r"Elevation $[\degree]$")
+ax.grid(True)
+plt.show()
 
