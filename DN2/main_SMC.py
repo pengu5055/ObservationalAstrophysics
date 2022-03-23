65d393ed9feebc131a870556e7c4620b741d5cf2
pengu5055 <urbancmarko1@gmail.com>
2022-03-23 22:59:17 +0100

    New SMC data

--- DN2/main_SMC.py
+++ DN2/main_SMC.py
@@ -7,7 +7,7 @@ c1, c2, c3 = cmr.take_cmap_colors("cmr.gothic", 3, cmap_range=(0.2, 0.8), return
 
 
 source_id, ra, dec, parallax, pmra, pmdec, phot_g_mean_mag = \
-    np.column_stack(np.genfromtxt("SMC_tc_rangemag5.csv", delimiter=",", skip_header=1))
+    np.column_stack(np.genfromtxt("SMC_rangemag_alt.csv", delimiter=",", skip_header=1))
 
 
 # ---- Proper motion filtering ----
@@ -22,47 +22,46 @@ def ellipse(u, v, x, y, a, b):
     return np.array(output)
 
 
-x_coord = 0.77
+x_coord = 0.75
 y_coord = -1.26
-filt = ellipse(pmra, pmdec, x_coord, y_coord, 0.5, 0.3)
+filt = ellipse(pmra, pmdec, x_coord, y_coord, 0.4, 0.25)
 ra_f = np.take(ra, filt)
 dec_f = np.take(dec, filt)
 pmra_f = np.take(pmra, filt)
 pmdec_f = np.take(pmdec, filt)
 pmra_n = pmra_f - np.median(pmra_f)
-pmdec_n = pmdec_f - np.median(pmdec_f)
-print(len(pmdec_n))
+pmdec_n = pmdec_f - np.median(pmdec_f) # len 49484
 C = np.hypot(pmra_n, pmdec_n)
 
 
 # ---- Proper motion filter plot ----
-# plt.scatter(pmra, pmdec, s=2, c=c1, label="Noise")
-# plt.scatter(pmra_f, pmdec_f, s=1, c="#ADF1D2", label="SMC stars")
-# plt.xlim(-0.25, 1.75)
-# plt.ylim(-2, -0.25)
-# plt.title("Filtering by proper motion")
-# plt.xlabel(r"$\Delta\alpha$ [mas/year]")
-# plt.ylabel(r"$\Delta\delta$ [mas/year]")
-# plt.legend()
-# plt.show()
+plt.scatter(pmra, pmdec, s=2, c=c1, label="Noise")
+plt.scatter(pmra_f, pmdec_f, s=1, c="#ADF1D2", label="SMC stars")
+plt.xlim(-0.25, 1.75)
+plt.ylim(-2, -0.25)
+plt.title("Filtering by proper motion")
+plt.xlabel(r"$\Delta\alpha$ [mas/year]")
+plt.ylabel(r"$\Delta\delta$ [mas/year]")
+plt.legend()
+plt.show()
 
 # ---- Proper motion quiver plot ----
-# plt.quiver(ra_f, dec_f, pmra_n, pmdec_n, C, width=0.004, scale=5, cmap="cmr.bubblegum", alpha=0.6)
-# plt.xlabel(r"$\alpha$ [$\degree$]")
-# plt.ylabel(r"$\delta$ [$\degree$]")
-# plt.title("Rotation of SMC")
-# plt.colorbar(label=r"$\Delta$ [mas/year]")
-# plt.show()
+plt.quiver(ra_f, dec_f, pmra_n, pmdec_n, C, scale=10, cmap="cmr.bubblegum", alpha=0.6)
+plt.xlabel(r"$\alpha$ [$\degree$]")
+plt.ylabel(r"$\delta$ [$\degree$]")
+plt.title("Rotation of SMC")
+plt.colorbar(label=r"$\Delta$ [mas/year]")
+plt.show()
 
 # ---- Stream plot code snippet from Ema with additions ----
 fig, ax = plt.subplots()
 x = ra_f
 y = dec_f
-u = pmra_n
-v = pmdec_n
+v = pmra_n
+u = pmdec_n
 
 # resample onto a 50x50 grid
-nx, ny = 90, 90
+nx, ny = 220, 220
 
 # (N, 2) arrays of input x,y coords and u,v values
 pts = np.vstack((x, y)).T
@@ -87,7 +86,7 @@ grid_C = np.hypot(ui, vi)
 # plot
 # fig, ax = plt.subplots(1, 1)
 # ax.hold(True)
-plt.streamplot(xi, yi, ui, vi, color=grid_C, cmap="cmr.bubblegum")
+plt.streamplot(xi, yi, -ui, -vi, color=grid_C, cmap="cmr.bubblegum")
 plt.title("Stream plot for SMC")
 plt.xlabel(r"$\alpha$ [$\degree$]")
 plt.ylabel(r"$\delta$ [$\degree$]")
