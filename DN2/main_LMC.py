7948baab614702cde22ad3401605a10f77457c2c
Marko Urbanč <urbancmarko1@gmail.com>
2022-03-23 12:37:17 +0100

    Beautiful rotation plots

--- DN2/main_LMC.py
+++ DN2/main_LMC.py
@@ -37,12 +37,14 @@ c1, c2, c3 = cmr.take_cmap_colors("cmr.gothic", 3, cmap_range=(0.2, 0.8), return
 source_id, ra, dec, parallax, pmra, pmdec, phot_g_mean_mag = \
     np.column_stack(np.genfromtxt("LMC_tc_rangemag5.csv", delimiter=",", skip_header=1))
 
+
 # ---- Proper motion filtering ----
-def elipse(pmra, pmdec, x, y, a, b):
+def ellipse(u, v, x, y, a, b):
+    """ Checks if (u, v) data point is in ellipse with center (x, y) and half axes a and b"""
     output = []
-    n = len(pmra)
+    n = len(u)
     for i in range(n):
-        if ((pmra[i] - x)**2)/a**2 + ((pmdec[i] - y)**2)/b**2 <= 1:
+        if ((u[i] - x) ** 2)/a**2 + ((v[i] - y) ** 2)/b**2 <= 1:
             output.append(i)
 
     return np.array(output)
@@ -50,10 +52,14 @@ def elipse(pmra, pmdec, x, y, a, b):
 
 x_coord = 1.86
 y_coord = 0.33
-filter = elipse(pmra, pmdec, x_coord, y_coord, 2.2, 1.7)
-pmra_f = np.take(pmra, filter)
-pmdec_f = np.take(pmdec, filter)
+filt = ellipse(pmra, pmdec, x_coord, y_coord, 0.7, 1)
+ra_f = np.take(ra, filt)
+dec_f = np.take(dec, filt)
+pmra_f = np.take(pmra, filt)
+pmdec_f = np.take(pmdec, filt)
 
+
+# ---- Proper motion filter plot ----
 plt.scatter(pmra, pmdec, s=2, c=c1, label="Noise")
 plt.scatter(pmra_f, pmdec_f, s=1, c=c2, label="LMC stars")
 plt.xlim(-4, 7)
@@ -65,19 +71,18 @@ plt.legend()
 plt.show()
 
 # ---- Proper motion quiver plot ----
-# pmra_median = np.median(pmra)
-# pmdec_median = np.median(pmdec)
-# pmra_sub = pmra - pmra_median
-# pmdec_sub = pmdec - pmdec_median
-#
-# C = np.hypot(pmra_sub, pmdec_sub)
-# # plt.scatter(pmra, pmdec, s=0.25, c=c2)
-#
-# plt.quiver(ra, dec, pmra_sub, pmdec_sub, C, scale=25, cmap="cmr.bubblegum", alpha=0.4)
-# plt.xlabel(r"$\alpha$ [$\degree$]")
-# plt.ylabel(r"$\delta$ [$\degree$]")
-# plt.title("Proper motions of LMC stars")
-# plt.show()
+pmra_n = pmra_f - np.median(pmra_f)
+pmdec_n = pmdec_f - np.median(pmdec_f)
+
+C = np.hypot(pmra_n, pmdec_n)
+
+plt.quiver(ra_f, dec_f, pmra_n, pmdec_n, C, width=0.002, headwidth=3, headlength=5,
+           scale=10, cmap="cmr.bubblegum", alpha=0.4)
+plt.xlabel(r"$\alpha$ [$\degree$]")
+plt.ylabel(r"$\delta$ [$\degree$]")
+plt.title("Rotation of LMC")
+plt.colorbar(label=r"$\Delta$ [mas/year]")
+plt.show()
 
 
 # ---- Stream plot (failed) ----
@@ -136,5 +141,5 @@ ui.shape = vi.shape = (ny, nx)
 # plot
 # fig, ax = plt.subplots(1, 1)
 # ax.hold(True)
-plt.streamplot(xi, yi, ui, vi, cmap="cmr.bubblegum")
+# plt.streamplot(xi, yi, ui, vi, cmap="cmr.bubblegum")
 plt.show()
