803ae2d00f2ca996db718bc06eba101c8f6ba58b
pengu5055 <urbancmarko1@gmail.com>
2022-04-26 00:43:47 +0200

    Spectra corrected but still shit

--- DN4/main_63700.py
+++ DN4/main_63700.py
@@ -24,19 +24,19 @@ def spec_line(wave_table, label_table, label_height=10000, label_offset=100):
 
 plt.figure(figsize=(12, 3))
 x, y = np.column_stack(np.genfromtxt("HD63700.txt"))
-x = x * 1e-10
 lines = ["5266.2", "5892", "6139", "6497.4", "6563.4", "6871.6", "7605.5", "7630.9", "8500.1", "8544", "8665.2"]
 label = ["Fe I", "Na I", "Cs III", "Fe I", "H I", "V I", "V I", "V I", "V I", "V I", "Fe I"]
 wavelengths = np.arange(1e-7, 1e-6, 1e-8)
-plt.plot(x, y/np.max(y), c=c1)
-# spec_line(lines, label)
+plt.plot(x, y, c=c1)
+spec_line(lines, label)
 # plt.axvline(6871.6, ls="--", c=c2)
 temp = 6000
-line2 = planck(wavelengths, temp)
-plt.plot(wavelengths, line2/np.max(line2), c=c3, label="Planck for {} K".format(temp))
+# line2 = planck(wavelengths, temp)
+# plt.plot(wavelengths, line2/np.max(line2), c=c3, label="Planck for {} K".format(temp))
 
-plt.title("Planck fitted to HD63700")
+plt.title("Some spectral lines of HD63700")
 plt.xlabel(r"$\lambda$ [m]")
-plt.ylabel("Normalized Intensity")
+plt.ylabel("Relative Intensity")
 plt.legend()
+plt.subplots_adjust(bottom=0.17)
 plt.show()
--- DN4/main_82734.py
+++ DN4/main_82734.py
@@ -31,13 +31,14 @@ lines = ["4861", "5168", "5266", "5890",  "5896", "6497", "6564", "6871", "7605"
 label = ["H I", "Fe I", "Fe I", "Na I", "Na I", "Fe I", "H I", "V I", "V I", "V I"]
 
 wavelengths = np.arange(1000e-10, 15000e-10, 1e-10)
-plt.plot(x, y/np.max(y), c=c1)
-# spec_line(lines, label)
+plt.plot(x, y, c=c1)
+spec_line(lines, label)
 # plt.axvline(6871.6, ls="--", c=c2)
 temp = 4000
-line2 = planck(wavelengths, temp)
-plt.plot(wavelengths * 10e9, line2/np.max(line2), c=c3, label=temp)
+# line2 = planck(wavelengths, temp)
+# plt.plot(wavelengths * 10e9, line2/np.max(line2), c=c3, label=temp)
 plt.title("Some spectral lines of HD82734")
 plt.xlabel(r"$\lambda$ [$\AA$]")
 plt.ylabel("Relative Intensity")
+plt.subplots_adjust(bottom=0.17)
 plt.show()
