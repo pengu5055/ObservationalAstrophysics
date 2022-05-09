5181760b3955324490a626262dfb801cb5941b68
pengu5055 <urbancmarko1@gmail.com>
2022-05-09 22:01:46 +0200

    Disttributions

--- DN5/main_05.py
+++ DN5/main_05.py
@@ -17,18 +17,25 @@ def cauchy_pdf(x, mu, gamma):
     return dist.pdf(x)
 
 
+def voigt(x, mu, sig, gamma):
+    """Returns normalized voigt profile"""
+    dist = np.convolve(cauchy_pdf())
+
+    return
+
 x_space = np.linspace(-3, 3, 100)
 x_space2 = np.linspace(-3, 3, 199)
 dist1 = cauchy_pdf(x_space, 0, 1)
 dist2 = gaussian(x_space, 0, 1)
-voigt = convolve(dist1, dist2)
-voigt = voigt/np.max(voigt)
-# voigt = voigt[:len(voigt)//2 + 1]
+dist3 = convolve(dist1, dist2)
+dist3 = dist3 / np.max(dist3)
+# dist3 = dist3[:len(dist3)//2 + 1]
 
 plt.plot(x_space, dist1, color=colors[4], label="Cauchy")
 plt.plot(x_space, dist2, color=colors[7], label="Gauss")
-plt.plot(x_space2, voigt, color=colors[14], label="Voigt")
+plt.plot(x_space2, dist3, color=colors[14], label="Voigt")
 
 plt.title("Distributions")
 plt.legend()
 plt.show()
+
---
+++ DN5/main_05_remaster.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cmasher as cmr
+from scipy.stats import cauchy
+
+colors = cmr.take_cmap_colors("cmr.torch", 20, cmap_range=(0.1, 0.9), return_fmt="hex")
+
+
+def gauss_pdf(x, mu, sig):
+    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
+
+
+def lorentz_pdf(x, mu, gamma):
+    dist = cauchy(mu, gamma)
+    return dist.pdf(x)
+
+
+def lorentz_voigt(x, A, xc, wL):
+    return (2*A / np.pi) * (wL / ((4*(x.astype(float) - xc)**2) + wL**2))
+
+
+def gauss_voigt(x, wG):
+    return np.sqrt((4*np.log(2)) / np.pi) * ((np.exp(-(((4*np.log(2)) / (wG**2))*(x.astype(float))**2))) / (wG))
+
+
+def voigt(x, xc, A, wG, wL):
+    """
+    y0 = offset, xc = center, A =area, wG = Gaussian FWHM, wL = Lorentzian FWHM
+    FWHM = 0.5346 * wL + sqrt(0.2166 * wL * wL + wG * wG)
+    Source: https://www.originlab.com/doc/Origin-Help/Voigt-FitFunc
+    """
+    dist = np.convolve(lorentz_voigt(x, A, xc, wL), gauss_voigt(x, wG), 'full')
+    return dist/np.max(dist)
+
+
+x_space = np.linspace(-3, 3, 100)
+x_space2 = np.linspace(-3, 3, 199)
+# dist1 = gauss_pdf(x_space, 0, 1)
+# dist2 = lorentz_pdf(x_space, 0, 1)
+# dist3 = voigt(x_space, 0, 1, 1, 1)
+
+# plt.plot(x_space, dist1, c=colors[4], label="Gauss")
+# plt.plot(x_space, dist2, c=colors[7], label="Lorentz")
+# plt.plot(x_space2, dist3, c=colors[14], label="Voigt")
+# plt.title("Distributions")
+# plt.xlabel("x")
+# plt.ylabel("f(x)")
+# plt.legend()
+# plt.show()
+
+plt.plot()
